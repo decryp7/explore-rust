874c1932af584a7f2fd73d12d59e8c6c59967a17
decryp7 <d3cryp7@gmail.com>
2024-07-26 21:54:58 +0800

    updated

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ mod build_version;
 fn main() {
 
     let pdfium = Pdfium::new(
-        Pdfium::bind_to_library(Pdfium::pdfium_platform_library_name_at_path("./"))
+        Pdfium::bind_to_library(Pdfium::pdfium_platform_library_name_at_path("./lib"))
             .or_else(|_| Pdfium::bind_to_system_library()).unwrap(),
     );
 
