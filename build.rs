951ff58e730e7c37d29758f96d3c1063d1f5d766
decryp7 <d3cryp7@gmail.com>
2024-07-26 22:02:31 +0800

    updated

--- build.rs
+++ build.rs
@@ -4,5 +4,5 @@ use copy_to_output::copy_to_output;
 fn main() {
     // Re-runs script if any files in res are changed
     println!("cargo:rerun-if-changed=lib/*");
-    copy_to_output("lib", &env::var("PROFILE").unwrap()).expect("Could not copy");
+    copy_to_output("lib/libpdfium.dylib", &env::var("PROFILE").unwrap()).expect("Could not copy");
 }
--- src/main.rs
+++ src/main.rs
@@ -9,8 +9,9 @@ mod build_version;
 
 fn main() {
 
+    //PDFium 128.0.6569.0
     let pdfium = Pdfium::new(
-        Pdfium::bind_to_library(Pdfium::pdfium_platform_library_name_at_path("./lib"))
+        Pdfium::bind_to_library(Pdfium::pdfium_platform_library_name_at_path("./"))
             .or_else(|_| Pdfium::bind_to_system_library()).unwrap(),
     );
 
