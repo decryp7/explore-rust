f8fa745671515f224c2f36be06c05b58bda48100
Kok Yong <d3cryp7@gmail.com>
2024-07-29 08:10:24 +0800

    add compilation based on OS

--- build.rs
+++ build.rs
@@ -4,5 +4,13 @@ use copy_to_output::copy_to_output;
 fn main() {
     // Re-runs script if any files in res are changed
     println!("cargo:rerun-if-changed=lib/*");
+
+    #[cfg(target_os = "macos")]
     copy_to_output("lib/libpdfium.dylib", &env::var("PROFILE").unwrap()).expect("Could not copy");
+
+    #[cfg(target_os = "windows")]
+    copy_to_output("lib/pdfium.dll", &env::var("PROFILE").unwrap()).expect("Could not copy");
+
+    #[cfg(target_os = "linux")]
+    copy_to_output("lib/libpdfium.so", &env::var("PROFILE").unwrap()).expect("Could not copy");
 }
